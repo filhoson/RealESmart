a218750063c08c0d4b98ebb58079fecc8b05a802
osboxes.org <osboxes@osboxes.Home>
2017-02-20 09:09:08 +0000

    Initial Commit

---
+++ RealEAssist/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name realEassistApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the realEassistApp
+ */
+angular.module('realEassistApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
